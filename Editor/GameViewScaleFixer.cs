f0226007262182f5b17e19504e05bf1608dc77f8
baba_s <shotababa1206@gmail.com>
2019-09-28 19:57:36 +0900

    first commit

---
+++ Editor/GameViewScaleFixer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace KoganeUnityLib
+{
+	[InitializeOnLoad]
+	public static class GameViewScaleFixer
+	{
+		private static readonly Assembly     GAME_VIEW_ASSEMBLY = typeof( EditorWindow ).Assembly;
+		private static readonly Type         GAME_VIEW_TYPE     = GAME_VIEW_ASSEMBLY.GetType( "UnityEditor.GameView" );
+		private static readonly BindingFlags BINDING_ATTRS      = BindingFlags.Instance | BindingFlags.NonPublic;
+		private static readonly Vector2      DEFAULT_SCALE      = new Vector2( 0.1f, 0.1f );
+
+		private static bool m_isUpdate;
+
+		static GameViewScaleFixer()
+		{
+			EditorApplication.update    += OnUpdate;
+			EditorApplication.delayCall += OnDelayCall;
+
+			UpdateGameViewScale();
+		}
+
+		private static void OnDelayCall()
+		{
+			m_isUpdate = true;
+		}
+
+		private static void OnUpdate()
+		{
+			if ( !m_isUpdate ) return;
+
+			UpdateGameViewScale();
+			m_isUpdate = false;
+		}
+
+		private static void UpdateGameViewScale()
+		{
+			var objects        = Resources.FindObjectsOfTypeAll( GAME_VIEW_TYPE );
+			var gameViewWindow = objects.FirstOrDefault() as EditorWindow;
+
+			if ( gameViewWindow == null ) return;
+
+			var zoomAreaField = GAME_VIEW_TYPE.GetField( "m_ZoomArea", BINDING_ATTRS );
+
+			if ( zoomAreaField == null ) return;
+
+			var zoomArea   = zoomAreaField.GetValue( gameViewWindow );
+			var scaleField = zoomArea.GetType().GetField( "m_Scale", BINDING_ATTRS );
+
+			if ( scaleField == null ) return;
+
+			scaleField.SetValue( zoomArea, DEFAULT_SCALE );
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Uni Game View Scale Fixer
+
+Game ビューの Scale を 1 に固定するエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-game-view-scale-fixer.svg)](https://github.com/baba-s/uni-game-view-scale-fixer/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-game-view-scale-fixer": "https://github.com/baba-s/uni-game-view-scale-fixer.git",
+```
+
+manifest.json に上記の記述を追加します  
+
+## 使い方
+
+![](https://cdn-ak.f.st-hatena.com/images/fotolife/b/baba_s/20190928/20190928195548.png)
+
+導入するだけで Game ビューの Scale が 1 に固定されます  
+正確にはスクリプトのコンパイル完了時や Unity 再生時に  
+Game ビューの Scale を 1 に変更します  
