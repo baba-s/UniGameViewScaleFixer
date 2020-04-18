4b5a685137d015117c75a37cad386a5de7c2c0af
baba_s <shotababa1206@gmail.com>
2020-04-18 13:26:29 +0900

    fix

--- Editor/GameViewScaleFixer.cs
+++ Editor/GameViewScaleFixer.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-namespace KoganeUnityLib
+namespace UniGameViewScaleFixer
 {
 	[InitializeOnLoad]
 	internal static class GameViewScaleFixer
